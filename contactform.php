6fefb23dab2ee22c8567c513f75e66e2b21cc2d7
meelis <mhass@itcollege.ee>
2020-04-08 18:29:18 +0300

    Improved contactform.php, added a backup fwrite that writes all submissions into a text file.

--- contactform.php
+++ contactform.php
@@ -8,15 +8,31 @@ if (isset($_POST['submit'])) {
     $mailFrom = $_POST['mail'];
     $message = $_POST['message'];
 
-    //Still need to set up the DB connect to actully test this mail ending, for now it will do nothing but display the php page.
+    //Still need to set up the DB connect to actully test this mail
 
-    $mailTo = "notrealmail@fake.net"
+    $mailTo = "notrealmail@fake.net";
     $headers = "From: ".$mailFrom;
     $txt = "Test mail from ".$name.".\n\n".$message;
 
-    //mail function is nice :D
+    //mail function is nice, but will be commented out till we do MSQL.
+    //mail($mailTo, $sub, $txt, $headers);
+    
+    //Leads back to  homepage, should probably make a page or JS pop up for confirmation, for now it will do.
+    //TODO confirmation page
+    header("Location: html/home.html?_ijt=88c49rui9mnfi0b87shpkua66e");
+    
+     //This should write the previous mail into a text file as a backup.
 
-    mail($mailTo, $sub, $txt, $headers);
-    header("Location: contact.php?mailsend");
-    echo "Worked!";
+    $writeFile = fopen("mailbackups.txt", "a+") or die ("Oopsie, you broke the file");
+
+
+    fwrite($writeFile, "".$name."\n");
+    fwrite($writeFile, "".$sub."\n");
+    fwrite($writeFile, "".$txt."\n");
+    fwrite($writeFile, "".$headers."\n");
+    fwrite($writeFile, "\n\n");
+    fclose($writeFile);
+
+
+
 }
