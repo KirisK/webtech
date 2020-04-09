fe70d43e78a3bd8a0a0b028dbf720d1a85b93c83
meelis <mhass@itcollege.ee>
2020-04-09 11:05:00 +0300

    Fixxed the footer blocking the button by using extra line spaces.

--- service.php
+++ service.php
@@ -4,6 +4,7 @@ if (isset($_POST['submit'])) {
 
 // for now the Service form just leads back to the homepage.
 // Thinking of making it basically a login with just a name, where you could see your listed service requests, but this would require MSQL, so it will be skipped for now.
+
     header("Location: html/redirect.html");
     header("Refresh: 5, Location: html/home.html?_ijt=88c49rui9mnfi0b87shpkua66e");
 
